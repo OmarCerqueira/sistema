6ee41ff9f596f2ff42cd1886f4c424cbd5222060
Omar Cerqueira <omarcerqueira12@gmail.com>
2023-11-27 12:16:10 -0300

    iteração com for e criação do objeto pessoa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sitema';
+  titulo = 'Lista de Pessoas';
+
+  pessoas = [
+    {
+      'nome' : 'Omar Cerqueira e Silva',
+      'endereco' : 'R Sabino Silva',
+      'email' : 'omar.cerqueira12@cerqueira.com',
+      'cpf' : '465.456.152-15'
+    },
+    {
+      'nome' : 'Lucas Santana',
+      'endereco' : 'R Paripe',
+      'email' : 'lucas.lu@cerqueira.com',
+      'cpf' : '489.594.258-95'
+    },
+    {
+      'nome' : 'Fernanda Oliveira',
+      'endereco' : 'R Demdezeiro',
+      'email' : 'fernanda-oli@cerqueira.com',
+      'cpf' : '487.569.589-25'
+    },
+    {
+      'nome' : 'Alex Pereira',
+      'endereco' : 'R Mont Serrat',
+      'email' : 'alex.al@cerqueira.com',
+      'cpf' : '458.623.548-21'
+    }
+  ]
 }
